41aefefc8cc14c1ae5af88b79b6fce6d15be551a
Tom Bear <tom@bearjam.dev>
2021-04-20 08:47:59 +0100

    ^^

--- src/components/CropImage.tsx
+++ src/components/CropImage.tsx
@@ -9,18 +9,19 @@ const CropImage = () => {
     THREE.TextureLoader,
     "https://images.unsplash.com/photo-1613910117442-b7ef140b37f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE4NTAxMzg5&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
   )
-  const [bar, setBar] = useState(0)
-  const { foo } = useSpring({
-    foo: bar,
+  const [v, setV] = useState(0)
+  const modV = () => void setV((p) => (p === 0 ? 0.2 : 0))
+
+  const { inset } = useSpring({
+    inset: [v, v, v, v],
   })
+
   return (
-    <mesh onClick={() => void setBar((p) => Number(!p))}>
+    <mesh onClick={modV}>
       <planeBufferGeometry args={[8, 6]} />
       <AnimatedCropImageShaderMaterial
         uniforms-u_image-value={imageTexture}
-        uniforms-u_inset-value={foo
-          .to((x) => 0 + x / 5)
-          .to((x) => [x, x, x, x])}
+        uniforms-u_inset-value={inset}
       />
     </mesh>
   )
