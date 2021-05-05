6248c9181e5995a5e54f5a083ff48a85a926d42b
Tom Bear <tom@bearjam.dev>
2021-05-05 17:00:51 +0100

    getting there

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,79 @@
+import { useSpring } from "@react-spring/core"
 import { OurAnimatedShaderMaterial } from "./OurShaderMaterial"
+import { useControls } from "leva"
 
 const App = () => {
+  const {
+    a,
+    b,
+    c,
+    d,
+    e: [e0, e1],
+    duration,
+  } = useControls({
+    a: {
+      min: 10,
+      max: 20,
+      value: 10,
+      step: 0.001,
+    },
+    b: {
+      min: 10,
+      max: 20,
+      value: 10,
+      step: 0.001,
+    },
+    c: {
+      min: 0,
+      max: 0.3,
+      value: 0.1,
+      step: 0.001,
+    },
+    d: {
+      min: -1,
+      max: 1,
+      value: 1,
+      step: 0.001,
+    },
+    e: {
+      min: 0,
+      max: 20,
+      step: 1,
+      value: [5, 15],
+    },
+    duration: {
+      min: 100,
+      max: 1000,
+      step: 100,
+      value: 1000,
+    },
+  })
+  const [{ e }] = useSpring(
+    {
+      e: e1,
+      from: {
+        e: e0,
+      },
+      loop: {
+        reverse: true,
+      },
+      config: {
+        duration,
+      },
+    },
+    [e0, e1, duration]
+  )
   return (
     <mesh rotation-x={Math.PI / 2} position-y={1}>
       <planeBufferGeometry args={[3, 3, 30, 30]} />
-      <OurAnimatedShaderMaterial wireframe />
+      <OurAnimatedShaderMaterial
+        uniforms-a-value={a}
+        uniforms-b-value={b}
+        uniforms-c-value={c}
+        uniforms-d-value={d}
+        uniforms-e-value={e}
+        wireframe
+      />
     </mesh>
   )
 }
