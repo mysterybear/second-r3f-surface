780924c4f2c9cc8bc18058cf347661cc7a2f7a2d
Tom Bear <tom@bearjam.dev>
2021-05-05 17:19:02 +0100

    spring stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,9 @@ const App = () => {
     c,
     d,
     e: [e0, e1],
-    duration,
+    mass,
+    tension,
+    friction,
   } = useControls({
     a: {
       min: 10,
@@ -37,15 +39,27 @@ const App = () => {
     },
     e: {
       min: 0,
-      max: 20,
-      step: 1,
-      value: [5, 15],
-    },
-    duration: {
-      min: 100,
       max: 1000,
-      step: 100,
-      value: 1000,
+      step: 1,
+      value: [0, 500],
+    },
+    mass: {
+      min: 0.1,
+      max: 2,
+      step: 0.001,
+      value: 1,
+    },
+    tension: {
+      min: 50,
+      max: 300,
+      step: 1,
+      value: 200,
+    },
+    friction: {
+      min: 10,
+      max: 200,
+      value: 30,
+      step: 1,
     },
   })
   const [{ e }] = useSpring(
@@ -58,10 +72,12 @@ const App = () => {
         reverse: true,
       },
       config: {
-        duration,
+        mass,
+        friction,
+        tension,
       },
     },
-    [e0, e1, duration]
+    [e0, e1, mass, tension, friction]
   )
   return (
     <mesh rotation-x={Math.PI / 2} position-y={1}>
